5b09b15b8f3bf7555fba2b607ded2c13040f2b1a
Usman Ghani <usmanghanishahid@gmail.com>
2025-01-28 01:41:32 +0500

    Include directories in package

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ const rl = readline.createInterface({
 
 console.log("Welcome to the Multi-Package CLI!\n");
 console.log("Choose an option:");
-console.log("1) API's");
-console.log("2) Simple Express Code");
+console.log("1) Crud-Operations");
+console.log("2) Sample-Code");
 
 rl.question("Enter your choice (1 or 2): ", (choice) => {
     let filename = '';
 
     if (choice === "1") {
-        filename = "generated-api.js";
+        filename = "Crud-Operations.js";
         fs.writeFileSync(filename, apiCode);
         console.log(`API code has been written to ${filename}`);
     } else if (choice === "2") {
-        filename = "generated-simple-code.js";
+        filename = "Sample-Code.js";
         fs.writeFileSync(filename, simpleCode);
         console.log(`Simple Express code has been written to ${filename}`);
     } else {
